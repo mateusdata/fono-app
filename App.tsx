050483731051c343f82871d7687623554bf5b525
Mateus Santos <mateuspele2015@gmail.com>
2023-11-19 18:46:59 -0300

    added: suport for tamaqui

--- App.tsx
+++ App.tsx
@@ -2,16 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthProvider from './src/context/ AuthProvider';
 import Routes from './src/routes/public/routes';
+import { TamaguiProvider } from 'tamagui';
+import config from './tamagui.config'
+import { Button, Text } from 'tamagui'
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar translucent={false}  backgroundColor='white' />
+      <StatusBar translucent={false} backgroundColor='white' />
       <AuthProvider>
-        <Routes />
+        <TamaguiProvider config={config}>
+          <Routes />
+          <Button onPress={()=>alert("Esse aqui é um botão tamaqui")} bg={"blue"}>
+            <Text col={"white"}>Botão tamaqui</Text>
+          </Button>
+        </TamaguiProvider>
+
       </AuthProvider>
-  </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
