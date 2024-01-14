49429fb5db4f8d863dcd1aa56e77c19bb67f10c2
Mateus Santos <mateuspele2015@gmail.com>
2024-01-14 01:47:44 -0300

    fix help

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
-import { Button, Linking, View } from 'react-native';
-
+import { View, Text, Pressable, Linking } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import CustomText from '../components/customText';
 const App = () => {
 
-  const handlePressWhatsAppMessage =  () => {
+  const handlePressWhatsAppMessage = () => {
     const message = 'Olá Doutora Flavia Neves, tudo bem? Estou usando o app fonotheapp e quero tirar algumas dúvidas.';
     const url = `whatsapp://send?phone=557599787828&text=${message}`;
-    const canOpen =  Linking.canOpenURL(url);
+    const canOpen = Linking.canOpenURL(url);
     if (canOpen) {
       Linking.openURL(url);
     } else {
-      console.log(`Don't know how to open this URL: ${url}`);
+      console.log(`Dnão suportado: ${url}`);
     }
   };
-
-  const handlePressWhatsAppMessage2 = async () => {
-    const message = 'Olá Doutora Flavia Neves, tudo bem? Estou usando o app fonotheapp e quero tirar algumas dúvidas.';
-    const url = `whatsapp://send?phone=557599787828&text=${message}`;
-    const canOpen = await Linking.canOpenURL(url);
-    if (canOpen) {
-      Linking.openURL(url);
-    } else {
-      console.log(`Don't know how to open this URL: ${url}`);
-    }
-  };
-
   return (
-    <View>
-      <Button title="usando função normal" onPress={handlePressWhatsAppMessage} />
-      <Button title="usando async await " onPress={handlePressWhatsAppMessage2} />
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    }}>
+      <CustomText style={{
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+      }}>Entre em contato conosco</CustomText>
+      <CustomText style={{
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 40,
+      }}>Nossa equipe está pronta para ajudá-lo. Clique no botão abaixo para nos enviar uma mensagem no WhatsApp.</CustomText>
+      <Pressable style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#25D366',
+        padding: 10,
+        borderRadius: 5,
+      }} onPress={handlePressWhatsAppMessage}>
+        <Ionicons name="logo-whatsapp" size={24} color="white" />
+        <CustomText style={{
+          color: 'white',
+          marginLeft: 10,
+        }}>Enviar mensagem</CustomText>
+      </Pressable>
     </View>
   );
 };
