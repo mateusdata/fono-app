da8aeab900d0405b351963ecb328567fa4c087c5
Mateus Santos <mateuspele2015@gmail.com>
2024-04-19 22:15:12 -0300

    refactor: In page of AnsweredQuestions

--- src/interfaces/globalInterface.ts
+++ src/interfaces/globalInterface.ts
@@ -3,11 +3,47 @@ export interface LoginInterface {
     senha: string;
 }
 export interface FormatPacient {
+    base_diseases: string;
+    consultation_reason: string;
+    food_profile: string;
+    chewing_complaint: string;
+    education: string;
+    status: string;
     person?: {
         first_name: string;
         last_name: string;
         cpf: string;
         birthday: string;
         gender: string;
-      };
+        per_id?: string;
+        use_id?: string;
+        created_at?: string;
+        updated_at: string
+    },
+    questionnaires: [
+        {
+            qus_id: number;
+            name: string;
+            purpose: string;
+            sections: [
+                {
+                    qhs_id: number;
+                    qus_id: number;
+                    name: string;
+                    questions:[
+                        {
+                            que_id:number;
+                            name:string;
+                            alternatives:any;
+                            answer:{
+                                qua_id: number;
+                                alternative:number;
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+
+    ]
 }
--- src/pages/AnsweredQuestions.tsx
+++ src/pages/AnsweredQuestions.tsx
@@ -9,183 +9,49 @@ import { Platform, Alert } from "react-native"
 import * as FileSystem from "expo-file-system"
 import * as Sharing from "expo-sharing"
 import dayjs from 'dayjs';
+import { FormatPacient } from '../interfaces/globalInterface';
+import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
 
 
 const AnsweredQuestions = () => {
-    const [expandedIndex, setExpandedIndex] = React.useState(null);
-    const [answered, setAnswered] = useState([]);
-    const { pac_id } = useContext(ContextPacient);
-
-    const [progressPercentage, setProgressPercentage] = useState(0)
-    const [isDownloading, setIsDownloading] = useState(false)
-
-    const date = dayjs(new Date()).format("DD-MM-YYYY-HH-mm-ss-SSS");
-    const PDF_NAME = `Relatório de anamnese - ${date}.pdf` 
-    const PDF_URI = `https://fono-api.vercel.app/generate-report/${pac_id}` // leve.
-    //const PDF_URI = "https://www.mcfadden.com.br/assets/pdf/Flofi.pdf" // pesado
-
-    const handlePress = (index) => {
-        setExpandedIndex(index === expandedIndex ? null : index);
-    };
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await api.get("answered-questionnaire/:pac_id");
-                console.log(response.data)
-                setAnswered(response.data);
-            } catch (error) {
-            }
-        };
-        fetchData();
-    }, []);
+  const [expandedIndex, setExpandedIndex] = React.useState(null);
+  const [answered, setAnswered] = useState([]);
+  const { pac_id } = useContext(ContextPacient);
+  const [pacient, setPacient] = useState<FormatPacient>();
 
 
-    function onDownloadProgress({
-        totalBytesWritten,
-        totalBytesExpectedToWrite,
-    }: FileSystem.DownloadProgressData) {
-        const percentage = (totalBytesWritten / totalBytesExpectedToWrite) * 100
-        setProgressPercentage(percentage)
-    }
-
-    async function getPdf() {
-        try {
-            setIsDownloading(true)
-
-            const fileUri = FileSystem.documentDirectory + PDF_NAME
-
-            const downloadResumable = FileSystem.createDownloadResumable(
-                PDF_URI,
-                fileUri,
-                {},
-                onDownloadProgress
-            )
-
-            const downloadResponse = await downloadResumable.downloadAsync()
-
-            if (downloadResponse?.uri) {
-                await fileSave(downloadResponse.uri, PDF_NAME )
-                setProgressPercentage(0)
-                setIsDownloading(false)
-            }
-        } catch (error) {
-            Alert.alert("Download", "Não foi possível realizar o download.")
-            console.error(error)
-        }
-    }
-
-    async function fileSave(uri: string, filename: string) {
-        if (Platform.OS === "android") {
-            // Pega a pasta temporária.
-            const directoryUri = FileSystem.cacheDirectory + filename
-
-            // Lê o conteúdo do arquivo em formato base64
-            const base64File = await FileSystem.readAsStringAsync(uri, {
-                encoding: FileSystem.EncodingType.Base64,
-            })
-
-            // Escreve o conteúdo do arquivo no diretório.
-            await FileSystem.writeAsStringAsync(directoryUri, base64File, {
-                encoding: FileSystem.EncodingType.Base64,
-            })
-
-            // Abre o arquivo recém-criado
-            await Sharing.shareAsync(directoryUri)
-        } else {
-            Sharing.shareAsync(uri)
-        }
-    }
-
-    return (
-        <View style={styles.container}>
-
-            <Pressable onPress={getPdf} style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 10, alignItems: "center" }}>
-                <Text>{typeof date}</Text>
-
-                <View>
-                    {progressPercentage > 0 && (
-                        <Text >
-                            {progressPercentage.toFixed(1)}% baixado...
-                        </Text>
-                    )}
-
-                    <AntDesign name="pdffile1" size={34} color="red" />
-                </View>
-            </Pressable>
-
-
-
-            <List.Section title='Perguntas respondidas' style={{ gap: 0 }}>
-                <List.Accordion
-                    title="Anamnese"
-                    style={{ backgroundColor: "#E8E8E8" }}
-                    left={(props) => <List.Icon {...props} icon="all-inclusive" />}
-                    expanded={expandedIndex === 1}
-                    onPress={() => handlePress(1)}>
-                    <List.Item title="Item 1" />
-                    <List.Item title="Item 2" />
-                    <List.Item title="Item 3" />
-                </List.Accordion>
-                <Text></Text>
-
-                <List.Accordion
-                    title="Analise estrutural"
-                    style={{ backgroundColor: "#E8E8E8" }}
-                    left={(props) => <List.Icon {...props} icon="cloud" />}
-                    expanded={expandedIndex === 2}
-                    onPress={() => handlePress(2)}>
-                    <List.Item title="Item 1" />
-                    <List.Item title="Item 2" />
-                    <List.Item title="Item 3" />
-                </List.Accordion>
-                <Text></Text>
-                <List.Accordion
-                    title="Analise funcional"
-                    style={{ backgroundColor: "#E8E8E8" }}
-                    left={(props) => <List.Icon   {...props} icon="filter" />}
-                    expanded={expandedIndex === 3}
-                    onPress={() => handlePress(3)}>
-                    <List.Item title="Item 1" />
-                    <List.Item title="Item 2" />
-                    <List.Item title="Item 3" />
-                </List.Accordion>
-
-
-            </List.Section>
-        </View>
-    );
-};
-
-export default AnsweredQuestions;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-        padding: 5,
-    }
-})
-
-
-
-
-/*
-
-import { useState } from "react"
-import { View, Text, Platform, Alert } from "react-native"
-
-import * as FileSystem from "expo-file-system"
-import * as Sharing from "expo-sharing"
-import { Button } from "react-native-paper"
-
-
-const PDF_NAME = "doc.pdf"
-const PDF_URI = "https://www.thecampusqdl.com/uploads/files/pdf_sample_2.pdf" // leve.
-//const PDF_URI = "https://www.mcfadden.com.br/assets/pdf/Flofi.pdf" // pesado
-
-export default function Download() {
   const [progressPercentage, setProgressPercentage] = useState(0)
   const [isDownloading, setIsDownloading] = useState(false)
+
+  const date = dayjs(new Date()).format("DD-MM-YYYY-HH-mm-ss-SSS");
+  const PDF_NAME = `Relatório de anamnese - ${date}.pdf`
+  const PDF_URI = `https://fono-api.vercel.app/generate-report/${pac_id}` // leve.
+  //const PDF_URI = "https://www.mcfadden.com.br/assets/pdf/Flofi.pdf" // pesado
+
+  const handlePress = (index) => {
+    setExpandedIndex(index === expandedIndex ? null : index);
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get(`/info-pacient/${pac_id}`)
+      setPacient(response.data);
+    }
+    fetchData()
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("answered-questionnaire/:pac_id");
+        console.log(response.data)
+        setAnswered(response.data);
+      } catch (error) {
+      }
+    };
+    fetchData();
+  }, []);
+
 
   function onDownloadProgress({
     totalBytesWritten,
@@ -244,17 +110,97 @@ export default function Download() {
   }
 
   return (
-    <View>
-    <Button  onPress={handleDownload} >PDF</Button>
+    <View style={styles.container}>
 
-      {progressPercentage > 0 && (
-        <Text >
-          {progressPercentage.toFixed(1)}% baixado...
-        </Text>
-      )}
+      <Pressable onPress={getPdf} style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 10, alignItems: "center" }}>
+
+        <View>
+          {progressPercentage > 0 && (
+            <Text >
+              {progressPercentage.toFixed(1)}% baixado...
+            </Text>
+          )}
+
+          <AntDesign name="pdffile1" size={34} color="red" />
+        </View>
+      </Pressable>
+
+
+
+      <List.Section title='Perguntas respondidas' style={{ gap: 0 }}>
+
+        <List.Accordion
+          title="Anamnese"
+          style={{ backgroundColor: "#E8E8E8" }}
+          left={(props) => <List.Icon {...props} icon="all-inclusive" />}
+          expanded={expandedIndex === 1}
+          onPress={() => handlePress(1)}>
+
+          <View style={{ left: -18, padding: 10, gap: 5 }}>
+            <Text style={{ fontSize: 18 }}>{`Doença base: ${pacient?.base_diseases}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Perfil alimentar: ${pacient?.food_profile}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Queixas de declutição: ${pacient?.chewing_complaint}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Doença bade: ${pacient?.education}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Doença bade: ${pacient?.consultation_reason}`}</Text>
+          </View>
+
+        </List.Accordion>
+        <Text></Text>
+
+        <List.Accordion
+          title="Analise estrutural"
+          style={{ backgroundColor: "#E8E8E8" }}
+          left={(props) => <List.Icon {...props} icon="cloud" />}
+          expanded={expandedIndex === 2}
+          onPress={() => handlePress(2)}>
+
+          {pacient?.questionnaires.map((item, index) => (
+            <View key={index} style={{ left: -18, padding: 10, gap: 5 }}>
+              <Text style={{ fontSize: 18 }}>{`Doença base: ${item?.name}`}</Text>
+              <Text style={{ fontSize: 18 }}>{`Doença base: ${item?.purpose}`}</Text>
+              {item.sections.map((item2) => (
+                <Text style={{ fontSize: 18 }}>{`Doença base: ${item2?.name}`}</Text>
+
+              ))}
+
+            </View>
+          ))}
+
+        </List.Accordion>
+        <Text></Text>
+
+        <List.Accordion
+          title="Analise funcional"
+          style={{ backgroundColor: "#E8E8E8" }}
+          left={(props) => <List.Icon   {...props} icon="filter" />}
+          expanded={expandedIndex === 3}
+          onPress={() => handlePress(3)}>
+
+          <View style={{ left: -18, padding: 10, gap: 5 }}>
+            <Text style={{ fontSize: 18 }}>{`Doença base: ${pacient?.base_diseases}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Perfil alimentar: ${pacient?.food_profile}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Queixas de declutição: ${pacient?.chewing_complaint}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Doença bade: ${pacient?.education}`}</Text>
+            <Text style={{ fontSize: 18 }}>{`Doença bade: ${pacient?.consultation_reason}`}</Text>
+          </View>
+
+        </List.Accordion>
+
+
+      </List.Section>
     </View>
-  )
-}
+  );
+};
+
+export default AnsweredQuestions;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 5,
+  }
+})
 
 
-*/
+
